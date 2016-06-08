0ff770721499db818ce4fe02b984bc077ae479bc
Grant Baker <grant.baker@colorado.edu>
2016-06-08 14:53:31 -0600

    Added Ramanujan, and started list of notable results

---
+++ MATLAB/NotablePlots.m
@@ -0,0 +1,4 @@
+%Notable plots that have been stumbled across
+
+%Branch point of the Ramanujan system (P, in q space)???
+ComplexPlot(ChazyPointsSimple(@RamanujanQ, [1,1,1i,1], 5, 0.01, 0.01, 10^10), 10^1)
