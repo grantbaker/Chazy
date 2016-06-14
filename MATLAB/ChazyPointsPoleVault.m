91efc6aa3eec718bd9a77686457ed72903c59763
Grant Baker <grant.baker@colorado.edu>
2016-06-14 13:49:19 -0600

    Total pole vaulting implemented, but absolutely nonfunctional

--- MATLAB/ChazyPointsPoleVault.m
+++ MATLAB/ChazyPointsPoleVault.m
@@ -1,7 +1,16 @@
 function [ out ] = ChazyPointsPoleVault(system, y0, r, h, th, tol)
-%UNTITLED4 Summary of this function goes here
-%   Detailed explanation goes here
+%CHAZYPOINTSPOLEVAULT Implements pole-vaulting techniques to generate
+%points in the complex plane of complex-valued differential equations
+%Returns a matrix of points, can be plotted with ComplexPlot
 
+y=[];
+y(1,:)=y0(:);
+
+for ang = 0:th:(2*pi)
+    y = [y;ChazyPointsLine(system,y0,r*exp(1i*ang),h,th,tol)];
+end
+
+out = y;
 
 end
 
