33aabdc1cdec0bf0f865437f244eb83218e1960c
Grant Baker <grant.baker@colorado.edu>
2016-07-28 15:59:22 -0600

    Pole consolidation

---
+++ MATLAB/PoleConsolidation.m
@@ -0,0 +1,9 @@
+function [ polesf ] = PoleConsolidation( polesi )
+%POLECONSOLIDATION Takes a set of poles 'polesi' and consolidates them
+%Returns a set of poles 'polesf' with all [0,...,0] removed as well as
+%duplicate poles averaged
+
+
+
+end
+
