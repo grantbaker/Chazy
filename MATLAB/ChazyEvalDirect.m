8b23fabc6ed54cf810d6ab712ab9d813819f0d15
grantbaker <grant.baker@colorado.edu>
2016-08-09 09:27:20 -0600

    GUI work, hopefully

--- MATLAB/ChazyEvalDirect.m
+++ MATLAB/ChazyEvalDirect.m
@@ -3,9 +3,9 @@ function [ result ] = ChazyEvalDirect(system, y0, y1)
 %returns result at y1
 
 %%{
-stepTol = 10^-3;
-minStep = 10^-10;
-minDelta = 10^-5;
+stepTol = 10^-1;
+minStep = 10^-5;
+minDelta = 10^-3;
 
 step = y1(1)-y0(1);
 %disp(abs(step));
